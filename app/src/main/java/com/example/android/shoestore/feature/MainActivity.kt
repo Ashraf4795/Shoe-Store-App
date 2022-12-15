998dc15c250bdbd445a82b686f5a2339be7acd2f
ashraf.hassan@vodafone.com <ashraf.hassan@vodafone.com>
2022-12-15 13:51:40 +0200

    init commit

---
+++ app/src/main/java/com/example/android/shoestore/feature/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.shoestore.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.shoestore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
